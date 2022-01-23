ce3eb0e42710d5a003af5840804fa6511b53268d
Doring <d0211468@kowon.dongseo.ac.kr>
2022-01-23 16:49:14 +0900

    이진 탐색 알고리즘의 재귀적 구현

---
+++ 재귀함수/63p-RecursiveBinarySearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int BSearchRecursive(int ar[], int first, int last, int target) {
+	int mid;
+	if (first > last)
+		return -1;  // Å½»ö ½ÇÆÐ
+	mid = (first + last) / 2;  //Å½»ö´ë»óÀÇ Áß¾Ó Ã£±â
+
+	if (ar[mid] == target)
+		return mid;
+	else if (target < ar[mid])
+		return BSearchRecursive(ar, first, mid - 1, target);
+	else
+		return BSearchRecursive(ar, mid+1, last, target);
+}
+
+int main() {
+	int arr[] = { 1,3,5,7,9 };
+	int idx;
+
+	idx = BSearchRecursive(arr, 0, sizeof(arr) / sizeof(int) - 1, 7);
+	if (idx == -1)
+		printf("Å½»ö ½ÇÆÐ.");
+	else
+		printf("Å½»ö ¼º°ø! Å¸°Ù ÀúÀå ÀÎµ¦½º: %d \n", idx);
+}
